458a9c6cb9b5ab6c89004e96c6157718f46f214d
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-01 21:22:38 -0400

    stuffs works finally

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Header from './Header'
-import Home from './Home.js';
-import Profile from './Profile';
-import Notifications from './Notifications';
-import Bookmarks from './Bookmarks';
-import {GlobalStyles} from './GlobalStyles'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home.js";
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+import Bookmarks from "./Bookmarks";
+import { GlobalStyles } from "./GlobalStyles";
+import TweetDetails from "./TweetDetails";
+import { CurrentUserContext } from "./CurrentUserContext";
 import styled from 'styled-components';
-import TweetDetails from './TweetDetails'
 
 const App = (props) => {
-  console.log('hello');
-  return(
-    <Router>
-      <GlobalStyles>
-      <Header/>
-      <Switch>
-        <Route exact path='/'><Home /></Route>
-        <Route path='/notifications'><Notifications/></Route>
-        <Route path='/bookmarks'><Bookmarks/></Route>
-        <Route path='/tweet/:tweetId'><TweetDetails/></Route>
-        <Route path='/:profileId'><Profile/></Route>
-      </Switch>
-      </GlobalStyles>
-    </Router>
-  )
-}
+  const { currentUser, status } = React.useContext(CurrentUserContext);
 
+  return (
+    <Wrapper>
+      {status === "loading" && <h2>Loading...</h2>}
+      {status === "idle" && (
+        <Router>
+          <GlobalStyles />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/notifications">
+              <Notifications />
+            </Route>
+            <Route path="/bookmarks">
+              <Bookmarks />
+            </Route>
+            <Route path="/tweet/:tweetId">
+              <TweetDetails />
+            </Route>
+            <Route path={`/profile/${currentUser.handle}`}>
+              <Profile />
+            </Route>
+          </Switch>
+        </Router>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  
+`
 
 export default App;
